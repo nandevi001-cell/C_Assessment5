a8fba63b00a8ef48c57646fdb508799ebbf283d3
nandevi001-cell <nandevi001@gmail.com>
2026-06-10 19:08:51 +0530

    Update C_program_16.c

--- C_program_16.c
+++ C_program_16.c
@@ -1,26 +1,23 @@
-#include <stdio.h>
-
+#include<stdio.h>
 int main()
 {
-    int n, i, count = 0;
-    printf("Enter a number: ");
-    scanf("%d", &n);
-
-    for(i = 1; i <= n; i++)
-    {
-        if(n % i == 0)
-        {
-            count = count + 1;
-        }
-    }
-    if(count == 2)
-    {
-        printf("Prime");
-    }
-    else
-    {
-        printf("Not Prime");
-    }
-
-    return 0;
+	int i,num,prime=1;
+	printf("Enter a number:");
+	scanf("%d",&num);
+	
+	if(num<=1)
+	{
+		prime=0;
+	}
+	for(i=2;i<num;i++)
+	{
+		if(num%i==0)
+		{
+			prime=0;
+		}
+	}
+	if(prime==1)
+	printf("The number is a prime number");
+	else
+	printf("The number is not a prime number");
 }
