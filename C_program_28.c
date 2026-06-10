a7f6105a202dd71452364ec43eabc759193ddbbc
nandevi001-cell <nandevi001@gmail.com>
2026-06-10 23:20:31 +0530

    Create C_program_28.c

---
+++ C_program_28.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+
+int main()
+{
+    int a, b, i;
+
+    printf("Enter two numbers: ");
+    scanf("%d%d", &a, &b);
+
+    for(i = (a > b ? a : b); ; i++)
+    {
+        if(i % a == 0 && i % b == 0)
+        {
+            printf("LCM = %d", i);
+            break;
+        }
+    }
+}
