32a4313cae352efa25ec749e4b243ab2adf87542
nandevi001-cell <nandevi001@gmail.com>
2026-06-10 22:06:11 +0530

    Update C_program_19.c

--- C_program_19.c
+++ C_program_19.c
@@ -1,35 +1,23 @@
-#include <stdio.h>
+#include<stdio.h>
 int main()
 {
-    int n, h, t, mid, i, flag;
-    printf("Enter a 4-digit number: ");
-    scanf("%d", &n)
-    h = (n / 100) % 10;
-    t = (n / 10) % 10;
-    mid = (h * 10) + t;
-    flag = 0;
-    for (i = 2; i < mid; i++)
-    {
-        if (mid % i == 0)
-        {
-            flag = 1;
-        }
-    }
-    for (i = 1; i <= 1; i++)
-    {
-        if (mid <= 1)
-        {
-            flag = 1;
-        }
-        if (flag == 0)
-        {
-            printf("Output - Prime");
-        }
-        else
-        {
-            printf("Output - Not Prime");
-        }
-    }
-
-    return 0;
+	int i,num,value,prime=1;
+	printf("Enter a number:");
+	scanf("%d",&num);
+	value=(num/10)%100;
+	if(value<=1)
+	{
+		prime=0;
+	}
+	for(i=2;i<value;i++)
+	{
+		if(value%i==0)
+		{
+			prime=0;
+		}
+	}
+	if (prime==1)
+	printf("%d is prime",value);
+	else
+	printf("%d is not prime",value);
 }
