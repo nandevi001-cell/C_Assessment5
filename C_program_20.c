c293e2b31a4c8893aedac40df54abd9462b7b7ee
nandevi001-cell <nandevi001@gmail.com>
2026-06-10 22:28:06 +0530

    Update C_program_20.c

--- C_program_20.c
+++ C_program_20.c
@@ -1,23 +1,28 @@
 #include<stdio.h>
+
 int main()
 {
-	int i,num,value,prime=1;
-	printf("Enter a number:");
-	scanf("%d",&num);
-	value=(num/10)%100;
-	if(value<=1)
-	{
-		prime=0;
-	}
-	for(i=2;i<value;i++)
-	{
-		if(value%i==0)
-		{
-			prime=0;
-		}
-	}
-	if (prime==1)
-	printf("%d is prime",value);
-	else
-	printf("%d is not prime",value);
+    int i, j, prime = 1, count = 0;
+
+    for(i = 0; i <= 9; i++)
+    {
+        prime = 1;
+
+        if(i <= 1)
+            prime = 0;
+
+        for(j = 2; j < i; j++)
+        {
+            if(i % j == 0)
+                prime = 0;
+        }
+
+        if(prime == 1)
+        {
+            count = count + 1;
+        }
+    }
+
+    printf("Total one-digit prime numbers = %d", count);
+
 }
